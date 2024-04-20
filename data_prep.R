00493dc1699794cb00bce89b291a28fd63dd2ff7
Casey O'Hara <cohara@ucsb.edu>
2024-04-19 21:11:00 -0700

    adding two more topics

--- data_prep.R
+++ data_prep.R
@@ -1,12 +1,9 @@
 library(tidyverse)
 library(synthesisr)
 
-fisheries_refs <- read_refs(here::here('data/individual_files/wos_fisheries_240419.bib'))
-forestry_refs <- read_refs(here::here('data/individual_files/wos_forestry_240419.bib'))
-water_refs <- read_refs(here::here('data/individual_files/wos_water_240419.bib'))
+refs_files <- list.files(here::here('data/individual_files'), pattern = '.bib', full.names = TRUE)
 
-all_refs <- bind_rows(fisheries_refs, forestry_refs, water_refs) %>%
-  distinct()
+all_refs <- read_refs(refs_files)
 
 write_refs(all_refs, format = 'bib', file = here::here('data/wos_240419.bib') )
   
